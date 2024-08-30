27f68b96a9cd79d95a3050785b72f52d4fea1be9
thecyberd3n <thebosscyberden@gmail.com>
2024-08-30 11:46:02 +0100

    changed how velocity works and kept sprint speed while in air

--- charactercontroller.gd
+++ charactercontroller.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 
 
 
-var SPEED = 10.0
+var SPEED = 1.0
 const JUMP_VELOCITY = 12
 const sensitivity = 0.4
 var targetFOV = 60
@@ -12,6 +12,11 @@ var oldRot = 0
 var direction = 0
 var input_dir =  0
 var friction = 0.7
+var groundSpeed = 0
+var vx = 0
+var vy = 0
+
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	$Camera3D.rotation_edit_mode = 2
@@ -34,63 +39,64 @@ func _physics_process(delta: float) -> void:
 		if velocity.y >= -10:
 			velocity.y -= 35 * delta
 
-	SPEED = 10.0
+
 	if Input.is_action_just_pressed("Jump") and (is_on_floor() or is_on_wall_only()):
 		velocity.y = JUMP_VELOCITY
-		print(get_wall_normal().x)
-		print((1 * int(rotation_degrees.y > 0)))
-		print(rotation_degrees.y)
-		if (1 * int(rotation_degrees.y > 0)):
-			print("left")
-		else:
-			print("right")
+		'''
 		if get_wall_normal().x == (1 * int(rotation_degrees.y+180 > 0)):
-			print("+5")
-			SPEED = 20.0
 			direction = (transform.basis * Vector3(5, 0, -2)).normalized()
-		elif get_wall_normal().x == (1 * int(rotation_degrees.y+180 > 0)):
-			print("+5")
-			SPEED = 20.0
-			direction = (transform.basis * Vector3(5, 0, -2)).normalized()
+		elif get_wall_normal().x == (1 * int(rotation_degrees.y+180 > 0)):WW
+			direction = (transform.basis * Vector3(5, 0, -2)).normalized()'''
 
 	if Input.is_action_just_pressed("Esc"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if Input.is_mouse_button_pressed( 1 ):
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	
+	
+	
+	
 	if Input.is_action_pressed("Sprint"):
-		SPEED = 20.0
+		SPEED = 3.5
 		targetFOV = 120
 	else:
 		targetFOV = 95
+		SPEED = 3
 	$Camera3D.set_fov($Camera3D.fov+((targetFOV-$Camera3D.fov)/15))
+	if is_on_floor():
+		groundSpeed = SPEED
+	
 
-	if Input.is_mouse_button_pressed( 1 ):
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-	targetRot = 0
 
-	#if Input.is_action_pressed("Forward"):
-
-		
-	if rotation_degrees.z > 10:
-		$Camera3D.rotation_degrees.z = 10
-	elif rotation_degrees.z < -10:
-		$Camera3D.rotation_degrees.z = -10
-	$Camera3D.rotate_z(deg_to_rad(((targetRot-$Camera3D.rotation_degrees.z)/10)))
-	if $Camera3D.rotation_degrees.z < 1 and $Camera3D.rotation_degrees.z > -1:
-		$Camera3D.rotation_degrees.z = 0
 
 	if get_wall_normal().x == 0:
 		input_dir = Input.get_vector("Left", "Right", "Forward", "Back")
 		direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if is_on_floor() or is_on_wall():
-		friction = 0.9
+		friction = 0.85
 	else:
-		friction = 0.3
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		SPEED = groundSpeed
+		friction = 0.95
+
+	if is_on_floor():
+		vx = direction.x * SPEED
+		vy = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, friction)
-		velocity.z = move_toward(velocity.z, 0, friction)
+		vx = direction.x * SPEED*0.7
+		vy = direction.z * SPEED*0.7
+	vx *= friction
+	vy *= friction
+	velocity.x += vx
+	velocity.z += vy
+
+	velocity.x *= friction
+	velocity.z *= friction
+	print(velocity.x)
+	print(velocity.z)
+	#velocity.x = move_toward(velocity.x, 0, friction)
+	#velocity.z = move_toward(velocity.z, 0, friction)
 
 	move_and_slide()
